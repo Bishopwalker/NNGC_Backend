6a923f9b66618d510231b773702944ec8f07863d
Bishop Walker <bishop.walker@sbd2.com>
2023-02-26 18:36:15 -0500

    Updates to Have the Application at my current state

--- src/main/java/com/northernneckgarbage/nngc/email/EmailService.java
+++ src/main/java/com/northernneckgarbage/nngc/email/EmailService.java
@@ -1,6 +1,7 @@
 package com.northernneckgarbage.nngc.email;
 
 import com.northernneckgarbage.nngc.repository.CustomerRepository;
+import io.jsonwebtoken.io.IOException;
 import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -8,10 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import com.sendgrid.*;
 
 @AllArgsConstructor
 @Service
-public class EmailService implements EmailSender {
+public class EmailService  implements EmailSender {
 
     private final JavaMailSender mailSender;
 
@@ -36,4 +38,28 @@ public class EmailService implements EmailSender {
         }
 
     }
+
+    @Override
+    public void sendWithSendGrid(String to, String subject, Content content) throws java.io.IOException {
+        Email from = new Email("bishop@northernneckgarbage.com");
+Email emailTo = new Email(to);
+      Mail mail = new Mail(from, subject, emailTo, content);
+
+        SendGrid sg = new SendGrid("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");
+        Request request = new Request();
+
+        try {
+          request.setMethod(Method.POST);
+          request.setEndpoint("mail/send");
+          request.setBody(mail.build());
+          Response response = sg.api(request);
+          System.out.println(response.getStatusCode());
+          System.out.println(response.getBody());
+          System.out.println(response.getHeaders());
+        } catch (java.io.IOException ex) {
+          throw ex;
+        }
+    }
+
+
 }
