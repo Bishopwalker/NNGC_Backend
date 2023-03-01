6886b3899bd29ccdfca48aed2cc997cfa7a5e2ec
Bishop Walker <bishop.walker@sbd2.com>
2023-03-01 11:30:45 -0500

    Updates to Have the Application at my current state

--- src/main/java/com/northernneckgarbage/nngc/email/EmailSender.java
+++ src/main/java/com/northernneckgarbage/nngc/email/EmailSender.java
@@ -4,6 +4,6 @@ import com.sendgrid.Content;
 import com.sendgrid.Email;
 
 public interface EmailSender {
-    void send(String to, String subject);
+
    void sendWithSendGrid(String to, String subject, Content content) throws java.io.IOException;
 }
--- src/main/java/com/northernneckgarbage/nngc/email/EmailService.java
+++ src/main/java/com/northernneckgarbage/nngc/email/EmailService.java
@@ -4,40 +4,26 @@ import com.northernneckgarbage.nngc.repository.CustomerRepository;
 import io.jsonwebtoken.io.IOException;
 import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import com.sendgrid.*;
 
-@AllArgsConstructor
+
 @Service
 public class EmailService  implements EmailSender {
 
-    private final JavaMailSender mailSender;
+    @Value("${spring.SENDGRID_API_KEY}")
+    private String sendGridApiKey;
+
 
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
-    //String link = " http://localhost:8080/auth/nngc/authenticate?token=" + token;
-    @Override
-    public void send(String to, String subject) {
 
-        LOGGER.info("Sending email to " + to);
-
-        try{
-            var message = mailSender.createMimeMessage();
-            var helper = new MimeMessageHelper(message, "UTF-8");
-            helper.setText(subject, true);
-            helper.setTo(to);
-            helper.setSubject("Confirm your email");
-            helper.setFrom("info@northernneckgarbage.com");
-            mailSender.send(message);
-
-    } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 
     @Override
     public void sendWithSendGrid(String to, String subject, Content content) throws java.io.IOException {
@@ -45,7 +31,9 @@ public class EmailService  implements EmailSender {
 Email emailTo = new Email(to);
       Mail mail = new Mail(from, subject, emailTo, content);
 
-        SendGrid sg = new SendGrid("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");
+        LOGGER.warn("API Key " + sendGridApiKey);
+        SendGrid sg = new SendGrid(sendGridApiKey);
+
         Request request = new Request();
 LOGGER.info("Sending email to " + to);
         try {
