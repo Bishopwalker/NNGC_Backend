602b405bb0ec9d47483a40f5a920e142c57f38d3
Bishop Walker <bishop.walker@sbd2.com>
2023-04-11 15:12:02 -0400

    Fixed the geoCode by ID

--- src/main/java/com/northernneckgarbage/nngc/google/GeocodingController.java
+++ src/main/java/com/northernneckgarbage/nngc/google/GeocodingController.java
@@ -3,6 +3,7 @@ package com.northernneckgarbage.nngc.google;
 import com.google.maps.errors.ApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/nngc")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173/*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*")
 public class GeocodingController {
 
     private final GeocodingService geocodingService;
@@ -21,8 +22,10 @@ public class GeocodingController {
         return geocodingService.getGeocoding(address);
     }
     @GetMapping("/geocoding/{id}")
-    public GeocodingData getGeocodingById(@PathVariable Long id) throws InterruptedException, ApiException, IOException {
-        return geocodingService.getGeocodeByID(id);
+    public ResponseEntity<GeocodingData> getGeocodingById(@PathVariable Long id) throws InterruptedException, ApiException, IOException {
+        log.info("Geocoding id: "+id);
+
+        return ResponseEntity.ok(geocodingService.getGeocodeByID(id));
     }
 
 }
