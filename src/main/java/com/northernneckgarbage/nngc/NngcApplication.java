eb62cc13032ed663c2143410af5e213afd127f1d
Bishop Walker <bishop.walker@sbd2.com>
2023-09-21 23:36:46 -0400

    test

--- src/main/java/com/northernneckgarbage/nngc/NngcApplication.java
+++ src/main/java/com/northernneckgarbage/nngc/NngcApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NngcApplication {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(NngcApplication.class, args);
     }
