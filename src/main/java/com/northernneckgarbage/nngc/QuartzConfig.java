7f6639b56f2957083e5b3d328b2764f88c8a16f1
Bishop Walker <walkerbishop333@gmail.com>
2024-05-27 18:50:37 -0400

    q

--- src/main/java/com/northernneckgarbage/nngc/QuartzConfig.java
+++ src/main/java/com/northernneckgarbage/nngc/QuartzConfig.java
@@ -1,40 +1,31 @@
 package com.northernneckgarbage.nngc;
-import com.northernneckgarbage.nngc.entity.HelloJob;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.TriggerBuilder;
-import org.quartz.JobBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
-@Configuration
-public class QuartzConfig {
-    @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob(HelloJob.class)
-                .withIdentity("helloJob", "group1")
-                .storeDurably()
-                .build();
-    }
-
-    @Bean
-    public Trigger trigger(JobDetail jobDetail) {
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail)
-                .withIdentity("helloTrigger", "group1")
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInSeconds(45)
-                        .repeatForever())
-                .build();
-    }
-
-    @Bean
-    public SchedulerFactoryBean schedulerFactory(Trigger trigger, JobDetail jobDetail) {
-        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
-        schedulerFactory.setJobDetails(jobDetail);
-        schedulerFactory.setTriggers(trigger);
-        return schedulerFactory;
-    }
-}
+//@Configuration
+//public class QuartzConfig {
+//    @Bean
+//    public JobDetail jobDetail() {
+//        return JobBuilder.newJob(HelloJob.class)
+//                .withIdentity("helloJob", "group1")
+//                .storeDurably()
+//                .build();
+//    }
+//
+//    @Bean
+//    public Trigger trigger(JobDetail jobDetail) {
+//        return TriggerBuilder.newTrigger()
+//                .forJob(jobDetail)
+//                .withIdentity("helloTrigger", "group1")
+//                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+//                        .withIntervalInSeconds(45)
+//                        .repeatForever())
+//                .build();
+//    }
+//
+//    @Bean
+//    public SchedulerFactoryBean schedulerFactory(Trigger trigger, JobDetail jobDetail) {
+//        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+//        schedulerFactory.setJobDetails(jobDetail);
+//        schedulerFactory.setTriggers(trigger);
+//        return schedulerFactory;
+//    }
+//}
