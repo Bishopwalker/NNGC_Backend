0619235fae7ee8199c0979257b64648c111fdb7d
Bishop Walker <bishop.walker@microhealthllc.com>
2025-01-24 23:52:35 -0500

    test

--- src/main/java/com/northernneckgarbage/nngc/NngcApplication.java
+++ src/main/java/com/northernneckgarbage/nngc/NngcApplication.java
@@ -5,22 +5,13 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.Key;
+
+
+
 
 @SpringBootApplication
 public class NngcApplication {
     public static void main(String[] args) {
         SpringApplication.run(NngcApplication.class, args);
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-
-
-
-
-//System.out.println(Base64.getEncoder().encodeToString(key.getEncoded()));
-    }}
-
- //Quickly run RouteService to test it
-//Auto load the DB with 1000 customers, make up address, and complete customer information
-
-
-
+    }
+}
